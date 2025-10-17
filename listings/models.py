d14b7c20947198407b472a37e912fa0f0787b760
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-17 22:37:24 +0300

    Update models.py

--- listings/models.py
+++ listings/models.py
@@ -54,3 +54,23 @@ class Review(models.Model):
     def __str__(self):
         return f"Review for {self.listing} by {self.user}"
 
+
+class Payment(models.Model):
+    """
+    Represents a payment.
+    """
+        STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Completed", "Completed"),
+        ("Failed", "Failed"),
+    ]
+
+    booking_reference = models.CharField(max_length=100, unique=True)
+    transaction_id = models.CharField(max_length=255, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
