8adb5e3a8621517ee3ff515efe5848449b4f06e1
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-18 00:01:00 +0300

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ListingViewSet, BookingViewSet, InitiatePaymentView
+from .views import ListingViewSet, BookingViewSet, InitiatePaymentView, VerifyPaymentView
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
@@ -11,4 +11,6 @@ router.register(r'bookings', BookingViewSet, basename='booking')
 urlpatterns = [
     path('', include(router.urls)),
     path('initiate-payment/', InitiatePaymentView.as_view(), name='initiate-payment'),
+    path('verify-payment/', VerifyPaymentView.as_view(), name='verify-payment'),
+
 ]
