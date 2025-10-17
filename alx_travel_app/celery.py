285c5aec76fb6ff2f62dfac6b5f1fe84db71aa97
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-18 00:11:48 +0300

    Update celery.py

--- alx_travel_app/celery.py
+++ alx_travel_app/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app0x02.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app.settings")
 
 app = Celery("alx_backend_graphql")
 app.config_from_object("django.conf:settings", namespace="CELERY")
